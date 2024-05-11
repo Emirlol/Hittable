ce01fe1596fe267a871fc896b223a1cbbb5c4ba6
Rime <81419447+Emirlol@users.noreply.github.com>
2024-05-11 22:22:41 +0300

    Add command to open the config screen

--- src/main/kotlin/me/lumiafk/hittable/Hittable.kt
+++ src/main/kotlin/me/lumiafk/hittable/Hittable.kt
@@ -2,11 +2,23 @@ package me.lumiafk.hittable
 
 import me.lumiafk.hittable.config.ConfigHandler
 import net.fabricmc.api.ClientModInitializer
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback
 
 class Hittable : ClientModInitializer {
     override fun onInitializeClient() {
         check(ConfigHandler.load()) { "Failed to load config." }
+        ClientCommandRegistrationCallback.EVENT.register { dispatcher, _ ->
+            dispatcher.register(
+                literal("hittable").then(literal("config").executes { context ->
+                    context.source.client.let {
+                        it.send {
+                            it.setScreen(ConfigHandler.createGui(it.currentScreen))
+                        }
+                    }
+                    1
+                })
+            )
+        }
     }
 }
