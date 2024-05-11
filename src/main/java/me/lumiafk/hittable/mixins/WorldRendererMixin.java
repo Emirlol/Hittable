8ed5fd81a3d42bce46b995c709431df89f658454
Rime <81419447+Emirlol@users.noreply.github.com>
2024-05-11 22:22:09 +0300

    Simplify outline logic and make the `enabled` option functional
    Why make set them as glowing when you can just mixin the glow check?

--- src/main/java/me/lumiafk/hittable/mixins/WorldRendererMixin.java
+++ src/main/java/me/lumiafk/hittable/mixins/WorldRendererMixin.java
@@ -1,29 +1,35 @@
 package me.lumiafk.hittable.mixins;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import me.lumiafk.hittable.config.ConfigHandler;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.OutlineVertexConsumerProvider;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 import java.awt.*;
 
 @Mixin(WorldRenderer.class)
 public abstract class WorldRendererMixin {
-    @ModifyExpressionValue(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;hasOutline(Lnet/minecraft/entity/Entity;)Z"))
-    private boolean hittable$shouldMobGlow(boolean original, @Local Entity entity) {
-        return entity.isGlowingLocal() || original;
+    @Final
+    @Shadow
+    private MinecraftClient client;
+
+    @WrapOperation(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;hasOutline(Lnet/minecraft/entity/Entity;)Z"))
+    private boolean hittable$hasOutline(MinecraftClient instance, Entity entity, Operation<Boolean> original) {
+	    return client.targetedEntity == entity && ConfigHandler.INSTANCE.getConfig().getEnabled();
     }
 
     @WrapOperation(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/OutlineVertexConsumerProvider;setColor(IIII)V"))
-    private void hittable$setGlowColor(OutlineVertexConsumerProvider instance, int red, int green, int blue, int alpha, Operation<Void> original, @Local LocalRef<Entity> entity) {
-        if (entity.get().isGlowingLocal()) {
+    private void hittable$setOutlineColor(OutlineVertexConsumerProvider instance, int red, int green, int blue, int alpha, Operation<Void> original, @Local LocalRef<Entity> entity) {
+        if (client.targetedEntity == entity.get() && ConfigHandler.INSTANCE.getConfig().getEnabled()) {
             Color color = ConfigHandler.INSTANCE.getConfig().getColor();
             instance.setColor(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha());
         } else {
