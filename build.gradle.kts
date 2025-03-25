761911158f8321fd30f644731f6edc136a1cc929
Rime <81419447+Emirlol@users.noreply.github.com>
2025-03-25 20:15:12 +0300

    Added support for 1.21.5
    Merely widened the version range, as there doesn't seem to be anything broken.

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,7 +56,7 @@ publishMods {
 	modrinth {
 		accessToken = providers.environmentVariable("MODRINTH_TOKEN")
 		projectId = "NxDKOEV1"
-		minecraftVersions.addAll("1.21.2", "1.21.3", "1.21.4")
+		minecraftVersions.addAll("1.21.2", "1.21.3", "1.21.4", "1.21.5")
 		requires("fabric-api")
 		requires("fabric-language-kotlin")
 		requires("yacl")
